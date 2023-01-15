2387bea7416a6bbb47f50b5469b037ef274ec550
salaza <gun159@outlook.com>
2023-01-15 16:19:35 +0700

    make request function for using api

--- TT-Project/Assets/Scripts/Database.cs
+++ TT-Project/Assets/Scripts/Database.cs
@@ -8,51 +8,72 @@ using UnityEngine.Networking;
 // Also try to download a non-existing page. Display the error.
 public class Database : MonoBehaviour
 {
-    private string URL = "https://tuxwkiinqssipykgyyau.supabase.co/rest/v1/Student?select=*";
+    private string DATABASE_URL = "https://tuxwkiinqssipykgyyau.supabase.co/rest/v1/";
     private string SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR1eHdraWlucXNzaXB5a2d5eWF1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzI0MTQ2MDMsImV4cCI6MTk4Nzk5MDYwM30.QUl9cKY1h-NRtPlLz2lQN0UOWUAWMSbAF6FnQF2Ga20";
-
-    private StudentList myStudentList = new StudentList();
+    private StudentList myStudentList;
 
     void Start()
     {
-        getAllData();
+        getTopTenStudentData(DATABASE_URL, SUPABASE_KEY);
     }
 
-    void Update()
+    // Method section.
+
+    // This function use to return all Student data.
+    public StudentList getStudentData(string ref_url, string key)
     {
-        
+        UnityWebRequest request = RequestURL_GET_AllStudenData(ref_url);
+        StartCoroutine(API_Coroutine(request, key));
+        return myStudentList;
     }
 
-    public void getAllData()
+    // Use to return top ten Student data.
+    public StudentList getTopTenStudentData(string ref_url, string key)
     {
-        StartCoroutine(Getdata_Coroutine(URL, SUPABASE_KEY));
+        UnityWebRequest request = RequestURL_GET_topTenStudent(ref_url);
+        StartCoroutine(API_Coroutine(request, key));
+        return myStudentList;
     }
 
-    IEnumerator Getdata_Coroutine(string url, string key)
+    // Request section
+
+    // Use to create web request for get all Student data.
+    private UnityWebRequest RequestURL_GET_AllStudenData(string ref_url)
     {
-        // make request for getting score
-        UnityWebRequest request = UnityWebRequest.Get(url);
+        string api_url = ref_url + "Student?select=*";
+        UnityWebRequest request = UnityWebRequest.Get(api_url);
+        return request;
+    }
+
+    private UnityWebRequest RequestURL_GET_topTenStudent(string ref_url)
+    {
+        string api_url = ref_url + "Student?limit=10&order=id.asc";
+        UnityWebRequest request = UnityWebRequest.Get(api_url);
+        return request;
+    }
+
+    // Do web request by follow request from input.
+    private IEnumerator API_Coroutine(UnityWebRequest request, string key)
+    {
+        // add important header to make request complete.
         request.SetRequestHeader("apikey", key);
         request.SetRequestHeader("Authorization", "Bearer " + key);
         yield return request.SendWebRequest();
 
-        //if request for some reason.
+        // if request is error for some reason.
         if (request.result == UnityWebRequest.Result.ConnectionError || request.result == UnityWebRequest.Result.DataProcessingError || request.result == UnityWebRequest.Result.ProtocolError)
         {
             Debug.LogError(request.error);
+            Debug.LogError("Check from this request: " + request.url);
             yield break;
         }
         // if request success
         else
         {
-            print("Get data successful.");
-
-            //print data
             string jsonData = "{\"students\":" + request.downloadHandler.text + "}";
 
+            print(jsonData);
             myStudentList = JsonUtility.FromJson<StudentList>(jsonData);
-            int studentID = myStudentList.students[0].id;
-            Debug.Log("Received: " + studentID);
         }
     }
 }
