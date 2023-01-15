bfef59d508a88f0c7058a29f784f791f53bf49b5
thidtanai <thidtanai_k@cmu.ac.th>
2023-01-15 18:20:04 +0700

    FirstPrototype_can play poor memory game

---
+++ TT-Project/Assets/Scripts/AddRememBtn.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddRememBtn : MonoBehaviour
+{
+    [SerializeField]
+    private Transform puzzlefield;
+
+    [SerializeField]
+    private GameObject button;
+
+    private void Awake()
+    {
+        if (button.tag == "PuzzleBtn")
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                //  create puzzle btn
+                GameObject _button = Instantiate(button);
+                _button.name = "" + i;  //  Rename button
+                _button.transform.SetParent(puzzlefield, false);
+            }
+        }else if (button.tag == "AnswerBtn")
+        {
+            for (int i = 4; i < 8; i++)
+            {
+                //  create puzzle btn
+                GameObject _button = Instantiate(button);
+                _button.name = "" + i;
+                _button.transform.SetParent(puzzlefield, false);
+            }
+        }
+        else
+        {
+            print("Tag error");
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ TT-Project/Assets/Scripts/GameManager.cs
@@ -0,0 +1,194 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    /*  SerialField variable */
+    [SerializeField]    // Puzzle image bg
+    private Sprite Puzzle_bgImage;
+
+    [SerializeField]    // Ans image bg
+    private Sprite Answer_bgImage;
+
+    /*  Global variable */
+    public Sprite[] puzzles;    //  puzzle image list
+
+    public List<Sprite> gamePuzzles = new List<Sprite>();
+
+
+    public List<Button> btns = new List<Button>();
+
+    private bool firstGuess, secondGuess;
+
+    private int countGuesses;
+    private int countCorrectGuesses;
+    private int gameGuesses;
+
+    private int firstGuessIndex, secondGuessIndex;
+
+    private string firstGuessPuzzle, secondGuessPuzzle;
+
+    /*  Method  */
+    private void Awake()
+    {
+        //  Get asset image from Resources
+        puzzles = Resources.LoadAll<Sprite>("Animal Basic Asset Pack/Free Sprites 1x");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetButton();
+        AddListeners();
+        AddGamePuzzles();
+        Shuffle(gamePuzzles);
+
+        gameGuesses = gamePuzzles.Count / 2;
+    }
+
+    void GetButton()
+    {
+        //  Change bgImage
+        //  Puzzle btn
+        GameObject[] Puzzle_objects = GameObject.FindGameObjectsWithTag("PuzzleBtn");
+
+
+        for (int i=0; i<Puzzle_objects.Length; i++)
+        {
+            btns.Add(Puzzle_objects[i].GetComponent<Button>()); //  Add btn to btnList
+            btns[i].image.sprite = Puzzle_bgImage;  // Change that btn bgImage
+
+        }
+
+        //  Ans btn
+        GameObject[] Answer_objects = GameObject.FindGameObjectsWithTag("AnswerBtn");
+
+        for (int i = 0; i < Answer_objects.Length; i++)
+        {
+            btns.Add(Answer_objects[i].GetComponent<Button>());
+            btns[i].image.sprite = Puzzle_bgImage;
+
+        }
+    }
+
+    void AddGamePuzzles()
+    {
+        //  Add puzzle image to gamePuzzle list
+        int looper = btns.Count;    //  check button count
+        int index = 0;
+
+        for (int i=0; i<looper; i++)    // for repeat image
+        {
+            if(index == looper / 2)
+            {
+                index = 0;
+            }
+            gamePuzzles.Add(puzzles[index]);
+            index++;
+        }
+    }
+    void AddListeners()
+    {
+        foreach(Button btn in btns)
+        {
+            btn.onClick.AddListener(() => PickPuzzle());    //  Use PickPuzzle when button has clicked
+        }
+    }
+
+    public void PickPuzzle()
+    {
+        
+        //  Check first pick is equal to second pick
+        if (!firstGuess)
+        {
+            firstGuess = true;
+            firstGuessIndex = int.Parse(UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.name);
+
+            firstGuessPuzzle = gamePuzzles[firstGuessIndex].name;
+
+            btns[firstGuessIndex].image.sprite = gamePuzzles[firstGuessIndex];
+        }
+        else if (!secondGuess)
+        {
+            secondGuess = true;
+            secondGuessIndex = int.Parse(UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.name);
+
+            secondGuessPuzzle = gamePuzzles[secondGuessIndex].name;
+
+            btns[secondGuessIndex].image.sprite = gamePuzzles[secondGuessIndex];
+            
+            if(firstGuessPuzzle == secondGuessPuzzle)
+            {
+                print("Puzzle Match");
+            }
+            else
+            {
+                print("Puzzle don't Match");
+            }
+
+            StartCoroutine(checkThePuzzleMatch());
+        }
+    }
+
+    //  Check selected is match
+    IEnumerator checkThePuzzleMatch()
+    {
+        yield return new WaitForSeconds(0.5f);
+
+        if (firstGuessPuzzle == secondGuessPuzzle)
+        {
+            //  if match set invissible
+            yield return new WaitForSeconds(0.5f);
+            btns[firstGuessIndex].interactable = false;
+            btns[secondGuessIndex].interactable = false;
+
+            btns[firstGuessIndex].image.color = new Color(0, 0, 0, 0);
+            btns[secondGuessIndex].image.color = new Color(0, 0, 0, 0);
+
+            CheckTheGameFinished();
+
+        }else
+        {
+            //  if not match return bgImage
+            btns[firstGuessIndex].image.sprite = Answer_bgImage;
+            btns[secondGuessIndex].image.sprite = Answer_bgImage;
+        }
+        yield return new WaitForSeconds(0.5f);
+
+        firstGuess = secondGuess = false;
+
+        void CheckTheGameFinished() {
+            countCorrectGuesses++;
+
+            if(countCorrectGuesses == gameGuesses)
+            {
+                print("game finished");
+                print("it took you " + countGuesses + " ");
+            }
+        }
+
+        
+    }
+    
+    //  Shuffel puzzle
+    void Shuffle(List<Sprite> list)
+    {
+        //  Puzzle Shuffle
+        for (int i = 0; i < list.Count/2; i++)
+        {
+            Sprite temp = list[i];
+            int randomIndex = Random.Range(i, list.Count/2);
+            list[i] = list[randomIndex];
+            list[randomIndex] = temp;
+        }
+        //  Answer Shuffle
+        for (int i = list.Count / 2; i < list.Count; i++)
+        {
+            Sprite temp = list[i];
+            int randomIndex = Random.Range(i, list.Count);
+            list[i] = list[randomIndex];
+            list[randomIndex] = temp;
+        }
+    }
+}
