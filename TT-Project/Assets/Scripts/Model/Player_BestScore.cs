072af1bb79b3ab3de2590aa789cdd0da0b33fec2
salaza <gun159@outlook.com>
2023-03-05 20:52:50 +0700

    create model for collect all player's bestscore

---
+++ TT-Project/Assets/Scripts/Model/Player_BestScore.cs
@@ -0,0 +1,11 @@
+[System.Serializable]
+public class Player_BestScore
+{
+    public string username;
+    public int best_score;
+}
+[System.Serializable]
+public class Player_BestScoreList
+{
+    public Player_BestScore[] jsonData;
+}
