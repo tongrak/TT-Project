b153d8856ae954eb110e9c6f854ac9d3bb1594f8
salaza <gun159@outlook.com>
2023-03-05 16:58:04 +0700

    create api for add new user in each game type score

--- TT-Project/Assets/Scripts/Register/Register.cs
+++ TT-Project/Assets/Scripts/Register/Register.cs
@@ -35,7 +35,11 @@ public class Register : MonoBehaviour
         string password = passwordField.text;
         string confirmPass = confirmPasswordField.text;
 
-        if(password != confirmPass)
+        if (password == null || confirmPass == null || username == null)
+        {
+            Debug.Log("Please fill in all text field");
+        }
+        else if(password != confirmPass)
         {
             Debug.Log("Password and ConfimPassword is not same");
         }
@@ -50,22 +54,52 @@ public class Register : MonoBehaviour
         // encoded password
         byte[] bytesToEncode = Encoding.UTF8.GetBytes(password);
         string encodedPassword = Convert.ToBase64String(bytesToEncode);
+        
         // create data for insert to database.
         Dictionary<string, string> newUser_data = new Dictionary<string, string>();
         newUser_data.Add("username", username);
         newUser_data.Add("password", encodedPassword);
-        // try to insert new player data.
+        
+        // try to insert new username&password to database.
         yield return dbConnector.API_POST_Coroutine(newUser_data, "Player_account");
-        // if username have already exist in database.
+        // if new username have already exist in database.
         if (dbConnector.errData != null)
         {
             Debug.Log("err: " + dbConnector.errData);
             Debug.Log("Can't register");
         }
-        // if username have never exist in database.
+        // if new username have never exist in database.
         else
         {
+            //insert new player to "Sequence score" table
+            Dictionary<string, string> newUser_seq_score = new Dictionary<string, string>();
+            newUser_seq_score.Add("username", username);
+            yield return dbConnector.API_POST_Coroutine(newUser_seq_score, "SequenceMem_Score");
+
+            //insert new player to "Reverse score" table
+            Dictionary<string, string> newUser_rev_score = new Dictionary<string, string>();
+            newUser_rev_score.Add("username", username);
+            yield return dbConnector.API_POST_Coroutine(newUser_rev_score, "Reverse_Score");
+
+            //insert new player to "MemoRandom score" table
+            Dictionary<string, string> newUser_memRand_score = new Dictionary<string, string>();
+            newUser_memRand_score.Add("username", username);
+            yield return dbConnector.API_POST_Coroutine(newUser_memRand_score, "MemoRandom_Score");
+
+            //insert new player to "Mix score" table
+            Dictionary<string, string> newUser_mix_score = new Dictionary<string, string>();
+            newUser_mix_score.Add("username", username);
+            yield return dbConnector.API_POST_Coroutine(newUser_mix_score, "Mix_Score");
+
+            newUser_data = null;
+            newUser_seq_score = null;
+            newUser_rev_score = null;
+            newUser_memRand_score = null;
+            newUser_mix_score = null;
             Debug.Log("register success!!!!!");
+
+            // change scene
+            ChangeSceneManager.changeScene("MainMenu");
         }
     }
 
@@ -89,6 +123,28 @@ public class Register : MonoBehaviour
         }
         else
         {
+            //insert new player to "Sequence score" table
+            Dictionary<string, string> newUser_seq_score = new Dictionary<string, string>();
+            newUser_seq_score.Add("username", username);
+            yield return dbConnector.API_POST_Coroutine(newUser_seq_score, "SequenceMem_Score");
+            //insert new player to "Reverse score" table
+            Dictionary<string, string> newUser_rev_score = new Dictionary<string, string>();
+            newUser_rev_score.Add("username", username);
+            yield return dbConnector.API_POST_Coroutine(newUser_rev_score, "Reverse_Score");
+            //insert new player to "MemoRandom score" table
+            Dictionary<string, string> newUser_memRand_score = new Dictionary<string, string>();
+            newUser_memRand_score.Add("username", username);
+            yield return dbConnector.API_POST_Coroutine(newUser_memRand_score, "MemoRandom_Score");
+            //insert new player to "Mix score" table
+            Dictionary<string, string> newUser_mix_score = new Dictionary<string, string>();
+            newUser_mix_score.Add("username", username);
+            yield return dbConnector.API_POST_Coroutine(newUser_mix_score, "Mix_Score");
+
+            newUser_data = null;
+            newUser_seq_score = null;
+            newUser_rev_score = null;
+            newUser_memRand_score = null;
+            newUser_mix_score = null;
             Debug.Log("register success!!!!!");
         }
     }
