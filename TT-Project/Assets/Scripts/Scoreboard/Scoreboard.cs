3b8d7a4a7fd9ee8719868ce87dc619061de879f0
salaza <gun159@outlook.com>
2023-03-05 22:28:31 +0700

    making scoreboard scene template

--- TT-Project/Assets/Scripts/Scoreboard/Scoreboard.cs
+++ TT-Project/Assets/Scripts/Scoreboard/Scoreboard.cs
@@ -7,27 +7,36 @@ public class Scoreboard : MonoBehaviour
     [SerializeField] private int maxScoreboardEntries = 10;
     [SerializeField] private Transform highscoreContainerTransform = null;
     [SerializeField] private GameObject scoreboardEntryObject = null;
+    [SerializeField] private GameObject currPlayerRankEntryObject = null;
+    [SerializeField] private string score_table;
+    [SerializeField] private StringSO usernameSO;
+    [SerializeField] private IntSO bestScoreSO;
     private PlayerList playerData;
     private SupabaseManager DBConnector;
+    private Player_BestScoreList allPlayer_bestScore;
+
+    private void Awake()
+    {
+        DBConnector = SupabaseManager.getInstance();
+    }
 
     private void Start()
     {
-        DBConnector = SupabaseManager.getInstance();
-        GetTopTenPlayer();
+        StartCoroutine(GetAllPlayerBestScore_Coroutine());
     }
 
     // Use to update scoreboard UI
     private void UpdateUI()
     {
         // Delete child object in HigscoreContainer
-        foreach(Transform child in highscoreContainerTransform)
+        foreach (Transform child in highscoreContainerTransform)
         {
             Destroy(child.gameObject);
         }
 
         // Create score list from 
         int rank = 1;
-        foreach(Player_Score highscore in playerData.jsonData.Take(maxScoreboardEntries))
+        foreach (Player_BestScore highscore in allPlayer_bestScore.jsonData.Take(maxScoreboardEntries))
         {
             // Instantiate use for cloning scoreboardEntryObject inside highscoreContainer to build ranking list.
             // GetComponent use for getting object that same type as ScoreboardEntryUI.
@@ -37,18 +46,29 @@ public class Scoreboard : MonoBehaviour
         }
     }
 
-    private void GetTopTenPlayer()
+    private IEnumerator GetAllPlayerBestScore_Coroutine()
     {
-        StartCoroutine(GetTopTenPlayer_Coroutine());
-    }
-
-    IEnumerator GetTopTenPlayer_Coroutine()
-    {
-        yield return DBConnector.API_GET_Coroutine("Player_Score?limit=10^&order=Best_score.desc");
-        Debug.Log(DBConnector.jsonData);
-        
+        // ดึงข้อมูล username และ best score ของแต่ละคนมาจาก supabase ใน table ที่เลือกมา
+        yield return DBConnector.API_GET_Coroutine(score_table+"?select=username,best_score&order=best_score.desc,username.asc");
         // นำข้อมูลใน jsonData มาแปลงเป็น class ของ C# โดยที่ตัวแปรใน class นั้นต้องมีชื่อที่ตรงกับ database แบบเป๊ะ ๆ
-        playerData = JsonUtility.FromJson<PlayerList>(DBConnector.jsonData);
+        allPlayer_bestScore = JsonUtility.FromJson<Player_BestScoreList>(DBConnector.jsonData);
+        Debug.Log("All player best score: " + DBConnector.jsonData);
+
         UpdateUI();
     }
+
+    //private void GetTopTenPlayer()
+    //{
+    //    StartCoroutine(GetTopTenPlayer_Coroutine());
+    //}
+
+    //IEnumerator GetTopTenPlayer_Coroutine()
+    //{
+    //    yield return DBConnector.API_GET_Coroutine("Player_Score?limit=10^&order=Best_score.desc");
+    //    //Debug.Log(DBConnector.jsonData);
+        
+    //    //// นำข้อมูลใน jsonData มาแปลงเป็น class ของ C# โดยที่ตัวแปรใน class นั้นต้องมีชื่อที่ตรงกับ database แบบเป๊ะ ๆ
+    //    //playerData = JsonUtility.FromJson<PlayerList>(DBConnector.jsonData);
+    //    UpdateUI();
+    //}
 }
