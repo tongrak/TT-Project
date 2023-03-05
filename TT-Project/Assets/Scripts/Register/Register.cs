915d86b0f7fc3f6c4b724994f93474f681cb62a5
salaza <gun159@outlook.com>
2023-03-05 17:35:41 +0700

    add back to 'login' scene button in 'register' scene

--- TT-Project/Assets/Scripts/Register/Register.cs
+++ TT-Project/Assets/Scripts/Register/Register.cs
@@ -35,7 +35,7 @@ public class Register : MonoBehaviour
         string password = passwordField.text;
         string confirmPass = confirmPasswordField.text;
 
-        if (password == null || confirmPass == null || username == null)
+        if (password == "" || confirmPass == "" || username == "")
         {
             Debug.Log("Please fill in all text field");
         }
@@ -66,7 +66,7 @@ public class Register : MonoBehaviour
         if (dbConnector.errData != null)
         {
             Debug.Log("err: " + dbConnector.errData);
-            Debug.Log("Can't register");
+            Debug.Log("This username have already exist");
         }
         // if new username have never exist in database.
         else
@@ -96,6 +96,14 @@ public class Register : MonoBehaviour
             newUser_rev_score = null;
             newUser_memRand_score = null;
             newUser_mix_score = null;
+
+            // save data to unity
+            UsernameSO.Value = username;
+            Seq_bestScoreSO.Value = 0;
+            MemRand_bestScoreSO.Value = 0;
+            Rev_bestScoreSO.Value = 0;
+            Mix_bestScoreSO.Value = 0;
+
             Debug.Log("register success!!!!!");
 
             // change scene
@@ -103,49 +111,49 @@ public class Register : MonoBehaviour
         }
     }
 
-    public void TestButton()
-    {
-        string username = "TestAPI";
-        string password = "1234";
-        StartCoroutine(Test_Coroutine(username, password));
-    }
+    //public void TestButton()
+    //{
+    //    string username = "TestAPI";
+    //    string password = "1234";
+    //    StartCoroutine(Test_Coroutine(username, password));
+    //}
 
-    IEnumerator Test_Coroutine(string username, string password)
-    {
-        Dictionary<string, string> newUser_data = new Dictionary<string, string>();
-        newUser_data.Add("username", username);
-        newUser_data.Add("password", password);
-        yield return dbConnector.API_POST_Coroutine(newUser_data, "Player_account");
-        if(dbConnector.errData != null)
-        {
-            Debug.Log("err: " + dbConnector.errData);
-            Debug.Log("Can't register");
-        }
-        else
-        {
-            //insert new player to "Sequence score" table
-            Dictionary<string, string> newUser_seq_score = new Dictionary<string, string>();
-            newUser_seq_score.Add("username", username);
-            yield return dbConnector.API_POST_Coroutine(newUser_seq_score, "SequenceMem_Score");
-            //insert new player to "Reverse score" table
-            Dictionary<string, string> newUser_rev_score = new Dictionary<string, string>();
-            newUser_rev_score.Add("username", username);
-            yield return dbConnector.API_POST_Coroutine(newUser_rev_score, "Reverse_Score");
-            //insert new player to "MemoRandom score" table
-            Dictionary<string, string> newUser_memRand_score = new Dictionary<string, string>();
-            newUser_memRand_score.Add("username", username);
-            yield return dbConnector.API_POST_Coroutine(newUser_memRand_score, "MemoRandom_Score");
-            //insert new player to "Mix score" table
-            Dictionary<string, string> newUser_mix_score = new Dictionary<string, string>();
-            newUser_mix_score.Add("username", username);
-            yield return dbConnector.API_POST_Coroutine(newUser_mix_score, "Mix_Score");
+    //IEnumerator Test_Coroutine(string username, string password)
+    //{
+    //    Dictionary<string, string> newUser_data = new Dictionary<string, string>();
+    //    newUser_data.Add("username", username);
+    //    newUser_data.Add("password", password);
+    //    yield return dbConnector.API_POST_Coroutine(newUser_data, "Player_account");
+    //    if(dbConnector.errData != null)
+    //    {
+    //        Debug.Log("err: " + dbConnector.errData);
+    //        Debug.Log("This username have already exist");
+    //    }
+    //    else
+    //    {
+    //        //insert new player to "Sequence score" table
+    //        Dictionary<string, string> newUser_seq_score = new Dictionary<string, string>();
+    //        newUser_seq_score.Add("username", username);
+    //        yield return dbConnector.API_POST_Coroutine(newUser_seq_score, "SequenceMem_Score");
+    //        //insert new player to "Reverse score" table
+    //        Dictionary<string, string> newUser_rev_score = new Dictionary<string, string>();
+    //        newUser_rev_score.Add("username", username);
+    //        yield return dbConnector.API_POST_Coroutine(newUser_rev_score, "Reverse_Score");
+    //        //insert new player to "MemoRandom score" table
+    //        Dictionary<string, string> newUser_memRand_score = new Dictionary<string, string>();
+    //        newUser_memRand_score.Add("username", username);
+    //        yield return dbConnector.API_POST_Coroutine(newUser_memRand_score, "MemoRandom_Score");
+    //        //insert new player to "Mix score" table
+    //        Dictionary<string, string> newUser_mix_score = new Dictionary<string, string>();
+    //        newUser_mix_score.Add("username", username);
+    //        yield return dbConnector.API_POST_Coroutine(newUser_mix_score, "Mix_Score");
 
-            newUser_data = null;
-            newUser_seq_score = null;
-            newUser_rev_score = null;
-            newUser_memRand_score = null;
-            newUser_mix_score = null;
-            Debug.Log("register success!!!!!");
-        }
-    }
+    //        newUser_data = null;
+    //        newUser_seq_score = null;
+    //        newUser_rev_score = null;
+    //        newUser_memRand_score = null;
+    //        newUser_mix_score = null;
+    //        Debug.Log("register success!!!!!");
+    //    }
+    //}
 }
