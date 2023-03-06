ba624da57645cc0a41cb9e3bcb0bde8eed6693cb
salaza <gun159@outlook.com>
2023-03-06 17:30:15 +0700

    create function for set position of best and recent score line

--- TT-Project/Assets/Scripts/Scoreboard/ScoreBarEntryUI.cs
+++ TT-Project/Assets/Scripts/Scoreboard/ScoreBarEntryUI.cs
@@ -11,8 +11,33 @@ public class ScoreBarEntryUI : MonoBehaviour
     [SerializeField] private TextMeshProUGUI recentText;
     [SerializeField] private TextMeshProUGUI bestText;
 
-    public void Initialise(int rank, Player_BestScore player)
+    // @param amountOfPlayer นั้นคือจำนวนผู้เล่นทั้งหมด(รวมทั้งตัว player ปัจจุบันด้วย)+1
+    public void Initialise(int recentScoreRank, int bestScoreRank, int amountOfPlayer)
     {
-        
+        RectTransform rt_bar = (RectTransform)bar.transform;
+        float barLength = rt_bar.rect.width;
+
+        float xPos_recent = (barLength / (amountOfPlayer - 1)) * (recentScoreRank - 1);
+        float xPos_best = (barLength / (amountOfPlayer - 1)) * (bestScoreRank - 1);
+
+        // set recent score line position
+        recentLine.transform.position = new Vector2(xPos_recent, recentLine.transform.position.y);
+        // set best score line position
+        bestLine.transform.position = new Vector2(xPos_best, bestLine.transform.position.y);
+
+        if(xPos_recent == xPos_best)
+        {
+            // set best&recent text position
+            bestText.transform.position = new Vector2(xPos_best, bestLine.transform.position.y);
+            bestText.text = "Best,Recent";
+        }
+        else
+        {
+            // set recent text position
+            recentLine.transform.position = new Vector2(xPos_recent, recentLine.transform.position.y);
+            // set best text position
+            bestLine.transform.position = new Vector2(xPos_best, bestLine.transform.position.y);
+
+        }
     }
 }
